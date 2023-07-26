ce76951e7e0995bf49ca8910538bca2b03b82068
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-26 19:30:21 +0300

    add branching middleware component to Program cs file

--- Program.cs
+++ Program.cs
@@ -1,29 +1,37 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.Use(async (context, next) => 
+//app.Use(async (context, next) => 
+//{
+//    await next();
+//    await context.Response.WriteAsync($"\n Status Code:  {context.Response.StatusCode}");
+//});
+//app.Use(async (context, next) =>
+//{
+//    if(context.Request.Path == "/short")
+//    {
+//        await context.Response.WriteAsync($"Request Short Circuited");
+//    }
+//    else
+//    {
+//        await next();
+//    }
+//});
+//app.Use(async (context, next) =>
+//{
+//    if ((context.Request.Method == HttpMethods.Get) && (context.Request.Query["custom"] == "true"))
+//    {
+//        context.Response.ContentType = "text/plain";
+//        await context.Response.WriteAsync("Custom Middleware \n");
+//    }
+//    await next();
+//});
+((IApplicationBuilder)app).Map("/branch", branch =>
 {
-    await next();
-    await context.Response.WriteAsync($"\n Status Code:  {context.Response.StatusCode}");
-});
-app.Use(async (context, next) =>
-{
-    if(context.Request.Path == "/short")
+    branch.UseMiddleware<Platform.QueryStringMiddleware>();
+    branch.Use(async (HttpContext context, Func<Task> next) => 
     {
-        await context.Response.WriteAsync($"Request Short Circuited");
-    }
-    else
-    {
-        await next();
-    }
-});
-app.Use(async (context, next) =>
-{
-    if ((context.Request.Method == HttpMethods.Get) && (context.Request.Query["custom"] == "true"))
-    {
-        context.Response.ContentType = "text/plain";
-        await context.Response.WriteAsync("Custom Middleware \n");
-    }
-    await next();
+        await context.Response.WriteAsync($"Branch Middleware");
+    });
 });
 app.UseMiddleware<Platform.QueryStringMiddleware>();
 app.MapGet("/", () => "Hello World!");
