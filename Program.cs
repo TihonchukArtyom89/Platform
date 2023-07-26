b19bf56f56716645791b12efe6ad96d120ae45cd
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-26 18:39:14 +0300

    fix custom middleware  component

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.Use(async (context, next) =>
 {
-    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == true)
+    if ((context.Request.Method == HttpMethods.Get) && (context.Request.Query["custom"] == "true"))
     {
         context.Response.ContentType = "text/plain";
         await context.Response.WriteAsync("Custom Middleware \n");
@@ -12,3 +12,12 @@ app.Use(async (context, next) =>
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+//app.Use(async (context, next) => {
+//    if (context.Request.Method == HttpMethods.Get
+//    && context.Request.Query["custom"] == "true")
+//    {
+//        context.Response.ContentType = "text/plain";
+//        await context.Response.WriteAsync("Custom Middleware \n");
+//    }
+//    await next();
+//});
