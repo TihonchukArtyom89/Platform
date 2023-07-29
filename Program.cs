344f39a1b44216d695520ee7b3517ae8d03fb7b6
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-29 09:20:30 +0300

    fix bug, remake Program cs with using class-based middleware components as endpoints

--- Program.cs
+++ Program.cs
@@ -44,10 +44,14 @@ var app = builder.Build();
 //app.UseMiddleware<Population>();
 //app.UseMiddleware<Capital>();
 app.UseRouting();
-app.UseEndpoints(endpoints => {
-    endpoints.MapGet("routing", async context => { await context.Response.WriteAsync("Request was routed");
+app.UseEndpoints(endpoints => 
+{
+    endpoints.MapGet("routing", async context =>
+    {
+        await context.Response.WriteAsync("Request was routed");
+    });
     endpoints.MapGet("capital/uk", new Capital().Invoke);
     endpoints.MapGet("population/paris", new Population().Invoke);
-    }); });
-app.Run(async context => { await context.Response.WriteAsync("Terminal Middleware Reached"); });
+});
+app.Run(async (context) => { await context.Response.WriteAsync("Terminal Middleware Reached"); });
 app.Run();
