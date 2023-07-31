3d8fb597b70228c4d937d2e7607a0d529da6354a
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-31 16:30:33 +0300

    change Program cs with adding middleware component and accessing endpoint in middleware component

--- Program.cs
+++ Program.cs
@@ -58,8 +58,21 @@ var app = builder.Build();
 //app.MapGet("capital/{country:countryName}", Capital.Endpoint);
 ////app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
 //app.MapGet("size/{city?}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
-app.Map("{number:int}", async context => { await context.Response.WriteAsync("Routed to the int endpoint"); }).Add(b => ((RouteEndpointBuilder)b).Order = 1);
-app.Map("{number:double}", async context => { await context.Response.WriteAsync("Routed to the double endpoint"); }).Add(b => ((RouteEndpointBuilder)b).Order = 2);
+app.Use(async (context,next) => 
+{
+    Endpoint? end = context.GetEndpoint();
+    if(end!=null)
+    {
+        await context.Response.WriteAsync($"{end.DisplayName} Selected\n");
+    }
+    else
+    {
+        await context.Response.WriteAsync("No Endpoint Selected\n");
+    }
+    await next();
+});
+app.Map("{number:int}", async context => { await context.Response.WriteAsync("Routed to the int endpoint"); }).WithDisplayName("Int Endpoint").Add(b => ((RouteEndpointBuilder)b).Order = 1);
+app.Map("{number:double}", async context => { await context.Response.WriteAsync("Routed to the double endpoint"); }).WithDisplayName("Double Endpoint").Add(b => ((RouteEndpointBuilder)b).Order = 2);
 app.MapFallback(async context => { await context.Response.WriteAsync("Routed to fallback endpoint"); });
 //app.UseEndpoints(endpoints => 
 //{
