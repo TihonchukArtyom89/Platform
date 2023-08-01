096c623b39875c47066c8cb6509c04cf3678c555
TihonchukArtyom89 <mortus1989@gmail.com>
2023-08-01 17:46:49 +0300

    change WeatherEndpoint cs with using dependency injection (use service with help of HttpContext object)

--- WeatherEndpoint.cs
+++ WeatherEndpoint.cs
@@ -1,9 +1,19 @@
-﻿namespace Platform;
+﻿using Platform.Services;
+
+namespace Platform;
 
 public class WeatherEndpoint
 {
     public static async Task Endpoint(HttpContext context)
     {
-        await context.Response.WriteAsync("Endpoint Class: It is cloudly in Milan");
+        IResponseFormatter? formatter = context.RequestServices.GetService<IResponseFormatter>();
+        if (formatter == null) 
+        { 
+            await context.Response.WriteAsync($"No service for {typeof(WeatherEndpoint)} found"); 
+        }
+        else
+        {
+            await formatter.Format(context, "Endpoint Class: It is cloudly in Milan");
+        }
     }
 }
