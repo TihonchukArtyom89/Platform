d960e39c78ccedd97a41117fef679b7f8f88ffdc
TihonchukArtyom89 <mortus1989@gmail.com>
2023-08-01 17:32:55 +0300

    change WeatherMiddleware cs to using dependency injection

--- WeatherMiddleware.cs
+++ WeatherMiddleware.cs
@@ -1,17 +1,21 @@
-﻿namespace Platform;
+﻿using Platform.Services;
+
+namespace Platform;
 
 public class WeatherMiddleware
 {
     private RequestDelegate next;
-    public WeatherMiddleware(RequestDelegate next)
+    private IResponseFormatter formatter;
+    public WeatherMiddleware(RequestDelegate nextDelegate, IResponseFormatter respFormatter)
     { 
-        this.next = next; 
+        next = nextDelegate; 
+        formatter = respFormatter;
     }
     public async Task Invoke(HttpContext context)
     {
         if(context.Request.Path=="/middleware/class")
         {
-            await context.Response.WriteAsync("Middleware Class: It is raining in London");
+            await formatter.Format(context,"Middleware Class: It is raining in London");
         }
         else
         {
