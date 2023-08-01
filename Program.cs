f43cfea58963b38e5ace3ed282b7bf1330f27f38
TihonchukArtyom89 <mortus1989@gmail.com>
2023-08-01 19:48:40 +0300

    change Program cs to use scoped service for demonstrate differencies between scoped and transient services

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Platform;
 using Platform.Services;
 var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
-builder.Services.AddTransient<IResponseFormatter, GuidService>();
+builder.Services.AddScoped<IResponseFormatter, GuidService>();
 var app = builder.Build();
 app.UseMiddleware<WeatherMiddleware>();
 //IResponseFormatter formatter = new TextResponseFormatter();
