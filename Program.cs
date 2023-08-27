440f183e31fdde4cbe2372c5afe62998b5be44a1
TihonchukArtyom89 <mortus1989@gmail.com>
2023-08-27 15:36:29 +0300

    modify Program cs to display detailes of http logging of the http requests and responces

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
  
 using Platform;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddHttpLogging(opts => { opts.LoggingFields = HttpLoggingFields.RequestMethod | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode; });
 var app = builder.Build();
 app.UseHttpLogging();
 //var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
