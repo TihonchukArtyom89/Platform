669da4714d15d45bccb21ca2255b7ea48b0b30e9
TihonchukArtyom89 <mortus1989@gmail.com>
2023-08-27 15:17:33 +0300

    change Program cs and appsettings Development json files to logging http requests and responces

--- Program.cs
+++ Program.cs
@@ -4,14 +4,15 @@ using Platform;
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
-logger.LogDebug("Pipeline configuration starting");
+app.UseHttpLogging();
+//var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+//logger.LogDebug("Pipeline configuration starting");
 app.MapGet("population/{city?}", Population.Endpoint);
 app.MapGet("/", async context =>
 {
     await context.Response.WriteAsync("Hello World");
 });
-logger.LogDebug("Pipeline configuration complete");
+//logger.LogDebug("Pipeline configuration complete");
 app.Run();
 
 
