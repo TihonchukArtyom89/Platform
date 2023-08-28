439d18e6c65a75e34b006712a7f4b590c708ae3b
TihonchukArtyom89 <mortus1989@gmail.com>
2023-08-28 18:12:53 +0300

    change Program cs to use configured UseStaticFiles middleware component, add folder staticfiles, add file hello html in folder static files

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
  
 using Platform;
 using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.FileProviders; 
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpLogging(opts => { opts.LoggingFields = HttpLoggingFields.RequestMethod | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode; });
 var app = builder.Build();
 app.UseHttpLogging();
 app.UseStaticFiles();
+var env = app.Environment;
+app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),RequestPath = "/files" }); 
 //var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
 //logger.LogDebug("Pipeline configuration starting");
 app.MapGet("population/{city?}", Population.Endpoint);
