775f3f909ce19ebe6093e5ba29b416ecdac19b59
TihonchukArtyom89 <mortus1989@gmail.com>
2023-09-05 09:29:45 +0300

    change Program cs to configure session and add session middleware component

--- Program.cs
+++ Program.cs
@@ -1,24 +1,11 @@
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.Configure<CookiePolicyOptions>(opts => { opts.CheckConsentNeeded = context => true; });
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(opts => { opts.IdleTimeout = TimeSpan.FromMinutes(30); opts.Cookie.IsEssential = true; });
 var app = builder.Build();
 app.UseCookiePolicy();
 app.UseMiddleware<Platform.ConsentMiddleware>();
-app.MapGet("/cookie", async context =>
-{
-    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
-    context.Response.Cookies.Append("counter1", counter1.ToString(), new CookieOptions { MaxAge = TimeSpan.FromMinutes(30), IsEssential = true });
-    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-    context.Response.Cookies.Append("counter2", counter1.ToString(), new CookieOptions { MaxAge = TimeSpan.FromMinutes(30) });
-    await context.Response.WriteAsync($"Counter 1: {counter1}, Counter 2: {counter2},");
-});
-app.MapGet("clear", context =>
-{
-    context.Response.Cookies.Delete("counter1");
-    context.Response.Cookies.Delete("counter2");
-    context.Response.Redirect("/");
-    return Task.CompletedTask;
-});
+app.UseSession();
 app.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
 
 app.Run();
@@ -60,7 +47,32 @@ app.Run();
 
 
 
+/*
+//chapter 16 use cookies
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<CookiePolicyOptions>(opts => { opts.CheckConsentNeeded = context => true; });
+var app = builder.Build();
+app.UseCookiePolicy();
+app.UseMiddleware<Platform.ConsentMiddleware>();
+app.MapGet("/cookie", async context =>
+{
+    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
+    context.Response.Cookies.Append("counter1", counter1.ToString(), new CookieOptions { MaxAge = TimeSpan.FromMinutes(30), IsEssential = true });
+    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+    context.Response.Cookies.Append("counter2", counter1.ToString(), new CookieOptions { MaxAge = TimeSpan.FromMinutes(30) });
+    await context.Response.WriteAsync($"Counter 1: {counter1}, Counter 2: {counter2},");
+});
+app.MapGet("clear", context =>
+{
+    context.Response.Cookies.Delete("counter1");
+    context.Response.Cookies.Delete("counter2");
+    context.Response.Redirect("/");
+    return Task.CompletedTask;
+});
+app.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
 
+app.Run();
+*/
 
 
 
