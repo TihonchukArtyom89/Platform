a0edd160a608d13f9c3ee88fd5fd7f986ce30d05
TihonchukArtyom89 <mortus1989@gmail.com>
2023-09-05 07:39:10 +0300

    change Program cs to use cookies

--- Program.cs
+++ Program.cs
@@ -2,7 +2,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-
+app.MapGet("/cookie", async context =>
+{
+    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
+    context.Response.Cookies.Append("counter1", counter1.ToString(), new CookieOptions { MaxAge = TimeSpan.FromMinutes(30) });
+    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+    context.Response.Cookies.Append("counter2", counter1.ToString(), new CookieOptions { MaxAge = TimeSpan.FromMinutes(30) });
+    await context.Response.WriteAsync($"Counter 1: {counter1}, Counter 2: {counter2},");
+});
+app.MapGet("clear", context =>
+{
+    context.Response.Cookies.Delete("counter1");
+    context.Response.Cookies.Delete("counter2");
+    context.Response.Redirect("/");
+    return Task.CompletedTask;
+});
 app.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
 
 app.Run();
