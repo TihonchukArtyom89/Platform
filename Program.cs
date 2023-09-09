bac75155abf7cd0cdfbd30e48c73485ba8f0d896
TihonchukArtyom89 <mortus1989@gmail.com>
2023-09-09 17:47:51 +0300

    add file ResponseStrings with class Responses, change Program cs to add status code middleware

--- Program.cs
+++ Program.cs
@@ -1,14 +1,42 @@
 //chapter 16 handling exceptions and errors
 var builder = WebApplication.CreateBuilder(args);
-
 var app = builder.Build();
-if(!app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/error.html");
     app.UseStaticFiles();
 }
-app.Run(context => { throw new Exception("Something gone wrong"); });
+app.UseStatusCodePages("text/html", Platform.Responses.DefaultResponse);
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/error")
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        await Task.CompletedTask;
+    }
+    else
+    {
+        await next();
+    }
+});
+app.Run(context =>
+{
+    throw new Exception("Something has gone wrong");
+});
 app.Run();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
