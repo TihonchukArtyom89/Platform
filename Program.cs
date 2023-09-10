ec87ababe299d35b4850fd385acd9ac195cc6788
TihonchukArtyom89 <mortus1989@gmail.com>
2023-09-10 17:46:36 +0300

    change Program cs to configure response caching, remake SumEndpoint cs to using caching responses

--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
 //chapter 17 caching data, persistent cache with db
+using Platform.Services;
 var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDistributedMemoryCache(opts => { opts.SizeLimit = 200; });
-builder.Services.AddDistributedSqlServerCache(opts => 
-{ 
-    opts.ConnectionString = builder.Configuration["ConnectionStrings:CacheConnection"]; 
+builder.Services.AddDistributedSqlServerCache(opts =>
+{
+    opts.ConnectionString = builder.Configuration["ConnectionStrings:CacheConnection"];
     opts.SchemaName = "dbo";
-    opts.TableName = "DataCache"; 
+    opts.TableName = "DataCache";
 });
+builder.Services.AddResponseCaching();
+builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
 var app = builder.Build();
+app.UseResponseCaching();
 app.MapEndpoint<Platform.SumEndpoint>("/sum/{count:int=1000000000}");
 app.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
 
 app.Run();
-
 
 
 
--- SumEndpoint.cs
+++ SumEndpoint.cs
@@ -1,6 +1,34 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
 namespace Platform;
 
+using Microsoft.AspNetCore.Routing;
+using Platform.Services;
+public class SumEndpoint
+{
+    public async Task Endpoint(HttpContext context,IDistributedCache cache,IResponseFormatter formatter,LinkGenerator generator)
+    {
+        int count;
+        int.TryParse((string?)context.Request.RouteValues["count"],out count);
+        long total = 0;
+        for (int i = 1; i <= count; i++)
+        {
+            total += i;
+        }
+        string totalString = $"({DateTime.Now.ToLongTimeString()}) {total}";
+        context.Response.Headers["Cache-Control"] = "public, max-age=120";
+        string? url = generator.GetPathByRouteValues(context, null, new { count = count });
+        await formatter.Format(context,$"<div>({DateTime.Now.ToLongTimeString()}) Total for {count} values:</div><div>{totalString}</div><a href={url}>Reload</a>");
+    }
+}
+
+
+
+
+
+/* //caching data value
+using Microsoft.Extensions.Caching.Distributed;
+namespace Platform;
+
 public class SumEndpoint
 {
     public async Task Endpoint(HttpContext context,IDistributedCache cache)
@@ -22,3 +50,4 @@ public class SumEndpoint
         await context.Response.WriteAsync($"({DateTime.Now.ToLongTimeString()}) Total for {count}  values:\n{totalString}\n");
     }
 }
+*/
