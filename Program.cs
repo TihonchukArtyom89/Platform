fb1e4192eb46d62adcfa6d7a95e329802b561819
TihonchukArtyom89 <mortus1989@gmail.com>
2023-09-10 09:09:23 +0300

    remake SumEndpoint cs to using caching service,change Program cs to set up cache service (add cache service)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 //chapter 17 caching data
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddDistributedMemoryCache(opts => { opts.SizeLimit = 200; });
 var app = builder.Build();
 app.MapEndpoint<Platform.SumEndpoint>("/sum/{count:int=1000000000}");
 app.MapGet("/",async context => { await context.Response.WriteAsync("Hello World!"); });
